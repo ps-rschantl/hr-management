6d7302b2424e7ea3784708203e8f049c1eddff77
Rene Schantl <schantl@parkside.at>
2019-11-12 23:33:36 +0100

    remove auto import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PageComponent } from './modules/projects/page/page.component';
 
 @NgModule({
-  declarations: [AppComponent, PageComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
 
